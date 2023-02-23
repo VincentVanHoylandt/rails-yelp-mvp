dc10a3899c2eaa464c708776130405b5a319b427
vincentvanhoylandt <vincent.vanhoylandt@gmail.com>
2023-02-23 09:26:13 +0100

    Not finished, not debugged

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+@restaurant = Restaurant.create([{name: "Chez Marie", address: "Lille", category: "french"}, {name: "Pho ", address: "Lomme", category: "chinese"}, {name: "Mamma mia", address: "Fives", category: "italian"}, {name: "Miasaki", address: "Lille", category: "japanese"}, {name: "Les Clochettes", address: "Bruxelles", category: "belgian"}])
